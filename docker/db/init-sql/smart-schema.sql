6e05b5aac33d4e797f2e60f96f75c6bd2900fe88
CalcYu <calcyu@qq.com>
2023-04-28 12:14:28 +0800

    chore: dockerfile update

---
+++ docker/db/init-sql/smart-schema.sql
@@ -0,0 +1,13 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+    
+create tablespace sup owner postgres location '/var/lib/postgresql/sup';
+create tablespace rtl owner postgres location '/var/lib/postgresql/rtl';
